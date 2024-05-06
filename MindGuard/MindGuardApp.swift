f0f45841d74e343b5b33de74e21ed26de00b6180
Raul Mena <>
2024-05-05 21:15:09 -0400

    Add new Xcode project

---
+++ MindGuard/MindGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindGuardApp.swift
+//  MindGuard
+//
+//  Created by Raul Mena on 5/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MindGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
